05ea12279c0d639af4a023cb5e040fb92b9e28d1
dharaneesh-006 <dharaneesh112@gmail.com>
2025-09-09 13:51:33 +0530

    git ignore

--- frontend/src/pages/Intro.jsx
+++ frontend/src/pages/Intro.jsx
@@ -1,36 +1,85 @@
 import gsap from 'gsap';
 import {useGSAP} from '@gsap/react';
-import React from 'react'
+import {React, useRef} from 'react'
 
 const Intro = () => {
 
-  useGSAP(()=>{
-    gsap.fromTo(
-      ".image",
-      { y: 200, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 2,
-        ease: "power3.out",
-      }
-    );
+  const container = useRef();
 
-    gsap.fromTo(
-      ".intro-text",
-      { y: 50, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 1.5,
-        stagger: 0.3,
-        ease: "power2.out",
-      }
-    );
-  },[])
+  useGSAP(
+    () => {
+      gsap.fromTo(
+        ".image",
+        { y: 200, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 2,
+          ease: "power1.out",
+          scrollTrigger: {
+            trigger: ".image",
+            start: "top 90%",
+            toggleActions: "play none none reverse",
+          },
+        }
+      );
+
+      gsap.fromTo(
+        ".intro-text",
+        { y: 50, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 2,
+          stagger: 0.3,
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: ".intro-text",
+            start: "top 90%",
+            toggleActions: "play none none reverse",
+          },
+        }
+      );
+      
+      gsap.fromTo(
+        ".image1",
+        { y: 200, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 2,
+          ease: "power1.out",
+          scrollTrigger: {
+            trigger: ".image1",
+            start: "top 80%",
+            toggleActions: "play none none reverse",
+          },
+        }
+      );
+
+      gsap.fromTo(
+        ".intro-text1",
+        { y: 50, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 2,
+          stagger: 0.3,
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: ".intro-text1",
+            start: "top 90%",
+            toggleActions: "play none none reverse",
+          },
+        }
+      );
+      
+    },
+    { scope: container }
+  );
 
   return (
-    <>
+    <div ref={container}>
       <section className="h-screen grid grid-cols-1 md:grid-cols-2 items-center justify-items-center gap-10 px-6 md:px-20">
         <div className="flex flex-row items-center justify-center">
           <img
@@ -53,7 +102,7 @@ const Intro = () => {
         </div>
       </section>
       <section className="h-screen grid grid-cols-1 md:grid-cols-2 items-center justify-items-center gap-10 px-6 md:px-20">
-        <div className="intro-text">
+        <div className="intro-text1">
           <p className="font-dancing font-bold text-5xl">
             Every year, billions of tons of food are wasted
             <br />
@@ -69,14 +118,14 @@ const Intro = () => {
             src="src/assets/branding/main1.png"
             alt=""
             width={400}
-            className="scale-x-[-1] image"
+            className="scale-x-[-1] image1"
           />
         </div>
       </section>
       <section></section>
       <section></section>
       <section></section>
-    </>
+    </div>
   );
 }
 
